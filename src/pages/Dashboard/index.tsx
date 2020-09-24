aea2d51f6ef07befda35c7d4ca03b2993f6f2545
araujogabriel77 <araujogabrielocn@gmail.com>
2020-09-23 23:23:35 -0300

    Dashboard finalizado

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,99 @@
+import React, { useState, FormEvent, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+  description: string;
+}
+
+/* Fc: Function Component */
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro ao tentar encontrar o repositório');
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  return (
+    <>
+      <img src={logoImg} alt="Github logo" />
+      <Title>Dashboard</Title>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      {inputError && <Error>{inputError}</Error>}
+      <Repositories>
+        {repositories.map((repository: Repository) => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/repository/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+/* Fc: Function Component */
+const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return <h1>Repository: {params.repository}</h1>;
+};
+
+export default Repository;
